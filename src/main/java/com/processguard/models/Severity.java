5a783866495637af290c77a2a0c605f48b12cacb
Pranav-Pudi <pranav.pudi1@gmail.com>
2026-04-05 11:11:36 +0800

    ffeat(model): add alert severity enum

---
+++ src/main/java/com/processguard/models/Severity.java
@@ -0,0 +1,12 @@
+package com.processguard.models;
+
+/**
+ * Severity levels for alerts in ProcessGuard.
+ * As defined in section 4.2 of the SDD.
+ */
+public enum Severity {
+    LOW,
+    MEDIUM,
+    HIGH,
+    CRITICAL
+}
