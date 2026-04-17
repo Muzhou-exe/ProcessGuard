87619f30eb9d2e4ad531859c07495f444a0b4136
chucksupasi <112365283+chucksupasi@users.noreply.github.com>
2026-04-17 23:36:58 +0800

    Code reuse declaration

--- src/main/java/com/processguard/Launcher.java
+++ src/main/java/com/processguard/Launcher.java
@@ -1,5 +1,11 @@
 package com.processguard;
 
+/*
+    We (Group #5) declare that we have used some AI-generated Javadoc comments and used AI to help debug
+    our code throughout different classes. We have also used AI to help with perfecting the markdown of the
+    user guides, develop guides and the planning documents (e.g., PRD, SDD).
+*/
+
 public class Launcher {
     public static void main(String[] args) {
         // This calls your actual main method
